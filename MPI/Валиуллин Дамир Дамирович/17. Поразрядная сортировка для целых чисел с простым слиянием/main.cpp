a8c83db02bb3243491ffdb9f13bf6ebc1de6925d
DamirV <32214748+DamirV@users.noreply.github.com>
2018-12-07 19:51:20 +0300

    testing

--- MPI/Валиуллин Дамир Дамирович/17. Поразрядная сортировка для целых чисел с простым слиянием/main.cpp
+++ MPI/Валиуллин Дамир Дамирович/17. Поразрядная сортировка для целых чисел с простым слиянием/main.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<string>
 #include<algorithm>
+#include<time.h>
 typedef unsigned int uint;
 
 void reset(uint *counter) {
@@ -52,16 +53,18 @@ void radix(uint* mas, uint* tempMas, int byte, int length) {
 	}
 }
 
-void sort(uint* mas, uint* tempMas, int length) {
+void sort(uint* mas, int length) {
+	uint* tempMas = new uint[length];
 	radix(mas, tempMas, 0, length);
 	radix(tempMas, mas, 1, length);
 	radix(mas, tempMas, 2, length);
 	radix(tempMas, mas, 3, length);
+	delete[] tempMas;
 }
 
 bool sravn(uint* mas, uint* tmas, int size) {
 	for (int i = 0; i < size; i++) {
-		std::cout << "IM HERE" << std::endl;
+
 		if (mas[i] != tmas[i]) {
 			return false;
 		}
@@ -69,34 +72,34 @@ bool sravn(uint* mas, uint* tmas, int size) {
 	return true;
 }
 
-void bond(uint* mas1, int size1, uint* mas2, int size2, uint* result){
+void bond(uint* mas1, int size1, uint* mas2, int size2, uint* result) {
 	int i = 0, j = 0, k = 0;
 	while (i < size1 && j < size2) {
-		
+
 		if (mas1[i] < mas2[j]) {
 			result[k] = mas1[i]; i++;
 		}
-		else { 
+		else {
 			result[k] = mas2[j];
 			j++;
-		} 
+		}
 		k++;
 	}
 
 	while (i < size1) {
 		result[k] = mas1[i];
 		k++; i++;
-	} 
+	}
 
 	while (j < size2) {
 		result[k] = mas2[j];
 		k++;
-		j++; 
-	} 
+		j++;
+	}
 }
 
-void main() {
-
+int main() {
+	srand(time(NULL));
 	int procSize, procRank;
 
 	MPI_Init(nullptr, nullptr);
@@ -104,107 +107,102 @@ void main() {
 	MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
 
 	int ROOT = 0;
-	const int size = 100000;
+	const int size = 10000000;
 	uint* mas = nullptr;
-	uint * tmas = nullptr;
+	uint* tmas = nullptr;
+	double gltime;
 	if (procRank == ROOT) {
-		 mas = new uint[size];
-		 //createMas(mas, size);
-		 uint* tmas = new uint[size];
-		 createMas(mas, tmas, size);
-		 std::sort(tmas, tmas + size);
-		/* проверка поразрядной сортировки
-
-		
-		uint* mas = new uint[size];
-		uint* tempMas = new uint[size];
-		createMas(mas, size);
-		
-		uint* tmas = new uint[size];
+		mas = new uint[size];
+		tmas = new uint[size];
 		createMas(mas, tmas, size);
-		double time = MPI_Wtime();
-		sort(mas, tempMas, size);
-		time = MPI_Wtime() - time;
-		std::cout << time << std::endl;
 
-	    time = MPI_Wtime();
-		std::sort(tmas, tmas + size);
-		time = MPI_Wtime() - time;
-		std::cout << time << std::endl;
-
-		std::cout << sravn(mas, tmas, size)<<std::endl;*/
-		
-		 
+		gltime = MPI_Wtime();
 	}
+
 	int* dis = new int[procSize];
 	int* scounts = new int[procSize];
-	
 
-	scounts[0] = size / procSize + size % procSize;
+
 	dis[0] = 0;
+	scounts[0] = size / procSize + size % procSize;
 
 	for (int i = 1; i < procSize; i++) {
-		scounts[i] = size / procSize;
-		dis[i] = procRank * size / procSize + size % procSize;
+		dis[i] = i * size / procSize + size % procSize;
+		scounts[i] = size / procSize ;
 	}
 
 	uint* result = new uint[scounts[procRank]];
-	uint* tempMas =  new uint[scounts[procRank]];
-	
-	MPI_Scatterv(mas, scounts,dis, MPI_UNSIGNED, result, scounts[procRank], MPI_UNSIGNED, ROOT, MPI_COMM_WORLD);
+
+	MPI_Scatterv(mas, scounts, dis, MPI_UNSIGNED, result, scounts[procRank], MPI_UNSIGNED, ROOT, MPI_COMM_WORLD);
 
 	double time = MPI_Wtime();
-	sort(result, tempMas, scounts[procRank]);
+	sort(result, scounts[procRank]);
 	time = MPI_Wtime() - time;
-	std::cout << "rank: " << procRank << " " << time << std::endl;
-
 	
 	int n = procSize;
 	int m = 1;
 
-	while (n > 1){
+	while (n > 1) {
 		n = n / 2 + n % 2;
 
-		if ((procRank - m) % (2 * m) == 0){
+		if ((procRank - m) % (2 * m) == 0) {
 
 			MPI_Send(&scounts[procRank], 1, MPI_UNSIGNED, procRank - m, 0, MPI_COMM_WORLD);
-
-			std::cout << "rank: " << procRank << " sending " << scounts[procRank] << " elements" << std::endl;
-
 			MPI_Send(result, scounts[procRank], MPI_UNSIGNED, procRank - m, 0, MPI_COMM_WORLD);
+
+			delete[] result;
 			MPI_Finalize();
-			return;
+			return 1;
 		}
-		if ((procRank % (2 * m) == 0) && (procSize - procRank > m)){
+		if ((procRank % (2 * m) == 0) && (procSize - procRank > m)) {
 			MPI_Status status;
 			int length;
 
 			MPI_Recv(&length, 1, MPI_UNSIGNED, procRank + m, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		
+
 			uint *y = new uint[length];
-			std::cout << "rank: " << procRank << " getting " << scounts[procRank + m] << " elements" << std::endl;
+			std::cout << "rank:" << procRank <<" get:" << scounts[procRank + m] << " elements, from:" <<status.MPI_SOURCE<<  std::endl;
 			MPI_Recv(y, length, MPI_UNSIGNED, procRank + m, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
 			uint* tempMas3 = new uint[length + scounts[procRank]];
-			bond(result, scounts[procRank] , y, length, tempMas3);
+			bond(result, scounts[procRank], y, length, tempMas3);
 
+			delete[] result;
+			delete[] y;
+	
 			result = new uint[length + scounts[procRank]];
 			for (int i = 0; i < length + scounts[procRank]; i++) {
 				result[i] = tempMas3[i];
 			}
-					
+			delete[] tempMas3;
 			scounts[procRank] += length;
-			std::cout << "rank: " << procRank << " size: " << scounts[procRank] << std::endl;
 		}
 		m *= 2;
 	}
 
 
-
 	if (procRank == ROOT) {
-		std::cout << sravn(result, tmas, size) << std::endl;
-		std::cout << scounts[0] << std::endl;
+		gltime = MPI_Wtime() - gltime;
+
+		std::cout << "Starting std::sort..." << std::endl;
+		time = MPI_Wtime();
+		sort(tmas, size);
+		std::sort(tmas, tmas + size);
+		time = MPI_Wtime() - time;
+		std::cout << "Sort on the " <<1<<" process: "<< time << " sec" << std::endl;
+		std::cout << "Sort on the "<< procSize << " process: " << gltime << " sec" << std::endl;
+
+		std::cout << "Acceleration: " << time / gltime << std::endl;
+		if (sravn(result, tmas, size)) {
+			std::cout << "ALL RIGHT" << std::endl;
+		}
+		else {
+			std::cout << "ERROR" << std::endl;
+		}
+		std::cout << "ROOT size: " << scounts[0] << std::endl;
+		
 	}
 
 	MPI_Finalize();
+	return 1;
 }
